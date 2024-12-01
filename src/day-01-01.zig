dde6a21d430d3b868b82ad586064e72ae1ae1f64
timimothy <timimothy@proton.me>
2024-12-01 19:36:05 +1030

    parse input and sort arrays

---
+++ src/day-01-01.zig
@@ -0,0 +1,57 @@
+const std = @import("std");
+
+const ArrayList = std.ArrayList;
+// Steps to solving this problem
+// 1. Split the input into two arrays
+// 2. Sort each array
+// 3. Iterate over each sorted array and sum their differences
+
+pub fn main() !void {
+    std.debug.print("Day 1 of advent of code\n", .{});
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const input =
+        \\3   4
+        \\4   3
+        \\2   5
+        \\1   3
+        \\3   9
+        \\3   3
+    ;
+
+    // Set up array lists to store data
+    var left = ArrayList(u16).init(allocator);
+    var right = ArrayList(u16).init(allocator);
+
+    // Split the input into lines
+    var lineIter = std.mem.split(u8, input, "\n");
+
+    // Iterate over each line to build left and right arrays
+    while (lineIter.next()) |line| {
+        var rowIter = std.mem.split(u8, line, "   ");
+
+        // Read first number
+        const leftInt = try std.fmt.parseInt(u16, rowIter.next().?, 10);
+        try left.append(leftInt);
+
+        // Read second number
+        const rightInt = try std.fmt.parseInt(u16, rowIter.next().?, 10);
+        try right.append(rightInt);
+    }
+
+    const leftArray = try left.toOwnedSlice();
+    const rightArray = try right.toOwnedSlice();
+
+    std.mem.sort(u16, leftArray, {}, std.sort.asc(u16));
+    std.mem.sort(u16, rightArray, {}, std.sort.asc(u16));
+    // For testing
+    for (leftArray) |row| {
+        std.debug.print("Left {d}\n", .{row});
+    }
+
+    for (rightArray) |row| {
+        std.debug.print("Right {d}\n", .{row});
+    }
+}
