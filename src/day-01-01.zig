fd4934be902bd11c397b8a1efcc07b1b077778c2
timimothy <timimothy@proton.me>
2024-12-04 07:53:13 +1030

    slight restructure

--- src/day-01-01.zig
+++ src/day-01-01.zig
@@ -13,12 +13,15 @@ const ArrayList = std.ArrayList;
 pub fn main() !void {
     std.debug.print("Day 1 of advent of code\n", .{});
 
+    // Set up stdin to be able to pipe in data
     const stdin = std.io.getStdIn().reader();
     const stat = try std.io.getStdIn().stat();
 
+    // Init Allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
+    // Create map to map numbers to their occurances
     var map = std.AutoHashMap(u32, u32).init(
         allocator,
     );
@@ -28,49 +31,43 @@ pub fn main() !void {
     var left = ArrayList(u32).init(allocator);
     var right = ArrayList(u32).init(allocator);
 
-    switch (stat.kind) {
-        std.fs.Dir.Entry.Kind.named_pipe => {
-            while (try stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', 4096)) |line| {
-                const trimmed_line = std.mem.trimRight(u8, line, "\n");
-                std.debug.print("'{s}'\n", .{trimmed_line});
-
-                var lineIter = std.mem.splitSequence(u8, trimmed_line, "   ");
-
-                const leftString = lineIter.next().?;
-                std.debug.print("'{s}'\n", .{leftString});
-                const leftInt = try std.fmt.parseInt(u32, leftString, 10);
-                std.debug.print("'{d}'\n", .{leftInt});
-                try left.append(leftInt);
-
-                const rightString = lineIter.next().?;
-                std.debug.print("'{s}'\n", .{rightString});
-                const rightInt = try std.fmt.parseInt(u32, rightString, 10);
-                try right.append(rightInt);
-
-                if (map.contains(rightInt)) {
-                    const value = map.get(rightInt).? + 1;
-                    try map.put(rightInt, value);
-                } else {
-                    try map.put(rightInt, 1);
-                }
-            }
-        },
-        else => {
-            return;
-        },
+    if (stat.kind != std.fs.Dir.Entry.Kind.named_pipe) {
+        std.debug.print("No piped data. Exiting\n", .{});
+        return;
     }
 
-    const leftArray = try left.toOwnedSlice();
-    const rightArray = try right.toOwnedSlice();
+    while (try stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', 4096)) |line| {
+        const trimmed_line = std.mem.trimRight(u8, line, "\n");
 
-    std.mem.sort(u32, leftArray, {}, std.sort.asc(u32));
-    std.mem.sort(u32, rightArray, {}, std.sort.asc(u32));
+        var lineIter = std.mem.splitSequence(u8, trimmed_line, "   ");
+
+        const leftString = lineIter.next().?;
+        std.debug.print("'{s}'\n", .{leftString});
+        const leftInt = try std.fmt.parseInt(u32, leftString, 10);
+        std.debug.print("'{d}'\n", .{leftInt});
+        try left.append(leftInt);
+
+        const rightString = lineIter.next().?;
+        std.debug.print("'{s}'\n", .{rightString});
+        const rightInt = try std.fmt.parseInt(u32, rightString, 10);
+        try right.append(rightInt);
+
+        if (map.contains(rightInt)) {
+            const value = map.get(rightInt).? + 1;
+            try map.put(rightInt, value);
+        } else {
+            try map.put(rightInt, 1);
+        }
+    }
+
+    std.mem.sort(u32, left.items, {}, std.sort.asc(u32));
+    std.mem.sort(u32, right.items, {}, std.sort.asc(u32));
 
     var totalDifference: u32 = 0;
     var score: u32 = 0;
 
-    for (leftArray, 0..) |leftValue, index| {
-        const rightValue = rightArray[index];
+    for (left.items, 0..) |leftValue, index| {
+        const rightValue = right.items[index];
 
         std.debug.print("{d} - {d}\n", .{ rightValue, leftValue });
         const difference = if (leftValue < rightValue) rightValue - leftValue else leftValue - rightValue;
