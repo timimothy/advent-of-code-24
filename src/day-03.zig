4b6a77faefdd493f7bf819cda9e7b6cd2b39cb0a
timimothy <timimothy@proton.me>
2024-12-04 07:33:09 +1030

    we got there. dont and do's

--- src/day-03.zig
+++ src/day-03.zig
@@ -2,13 +2,7 @@ const std = @import("std");
 
 const ArrayList = std.ArrayList;
 
-// Steps to solving this problem
-// 1. Split the input into two arrays
-// 2. Sort each array
-// 3. Iterate over each sorted array and sum their differences
-
-// Part 2
-// When iterating over the
+// var token: Token = Token.none;
 
 pub fn main() !void {
     std.debug.print("Day 1 of advent of code\n", .{});
@@ -23,51 +17,38 @@ pub fn main() !void {
 
     switch (stat.kind) {
         std.fs.Dir.Entry.Kind.named_pipe => {
-            while (try stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', 999999)) |buff| {
-                var parts = std.mem.splitSequence(u8, buff, "mul");
+            var list = ArrayList([]const u8).init(allocator);
+            defer list.deinit();
 
-                while (parts.next()) |part| {
-                    std.debug.print("\"{s}\": ", .{part});
+            var enabled = true;
+            const input = try stdin.readAllAlloc(allocator, 9999999999);
 
-                    if (part.len == 0) continue;
-                    if (part[0] != '(') {
-                        // Not valid
-                        std.debug.print("skip\n", .{});
-                        continue;
-                    }
+            //   total += try processBuf(allocator, buff);
+            var cursor: usize = 0;
 
-                    const index = std.mem.indexOfPos(u8, part, 1, &[_]u8{')'}) orelse {
-                        std.debug.print("no )\n", .{});
-                        continue;
-                    };
+            std.debug.print("Len {d}\n", .{input.len});
+            while (cursor < input.len) {
+                switch (enabled) {
+                    true => {
+                        const dontStart = std.mem.indexOfPos(u8, input, cursor, "don't()") orelse input.len;
 
-                    var numbers = std.mem.splitSequence(u8, part[1..index], ",");
-
-                    var list = ArrayList(u64).init(allocator);
-                    var err = false;
-                    while (numbers.next()) |text| {
-                        std.debug.print("'{s}', ", .{text});
-                        const valid = validString(text);
-
-                        if (!valid) {
-                            err = true;
-                            break;
-                        }
-
-                        const number: u64 = std.fmt.parseInt(u64, text, 10) catch continue;
-                        try list.append(number);
-                    }
-
-                    if (list.items.len != 2 or err) {
-                        std.debug.print("incorrect numbers\n", .{});
-                        continue;
-                    }
-
-                    const mult = (list.items[0] * list.items[1]);
-
-                    total += mult;
-                    std.debug.print("{d}\n", .{total});
+                        try list.append(input[cursor..dontStart]);
+                        cursor = dontStart + 7;
+                        enabled = false;
+                    },
+                    false => {
+                        const doStart = std.mem.indexOfPos(u8, input, cursor, "do()") orelse input.len;
+                        //     try list.append(input[cursor..doStart]);
+                        cursor = doStart + 4;
+                        enabled = true;
+                    },
                 }
+            }
+
+            for (list.items) |row| {
+                std.debug.print("{s}\n", .{row});
+
+                total += process_string(allocator, row);
             }
         },
         else => {
@@ -90,4 +71,164 @@ fn validString(text: []const u8) bool {
 test "check if string is valid" {
     const valid = validString("(959]+: (510,621){/$ %from(535,780)<");
     try std.testing.expectEqual(false, valid);
+}
+
+// const Token = enum { mul, dont, do, none };
+
+// const tokens = [_]Token{ Token.do, Token.dont, Token.mul };
+
+// pub fn processBuf(alloc: std.mem.Allocator, input: []const u8) !u32 {
+//     var cursor: usize = 0;
+//     var total: u32 = 0;
+//     std.debug.print("{s}:\n", .{input});
+//     while (cursor < input.len) {
+//         std.debug.print("Cursor: {d} | Token: {any}\n", .{ cursor, token });
+//         if (token == Token.mul) {
+
+//             //  std.debug.print("{s}: End Bracket {d}\n", .{ input[cursor..end], end });
+
+//             var parts = std.mem.splitSequence(u8, input[cursor..end], ",");
+
+//             var numList = ArrayList(u32).init(alloc);
+//             var err = false;
+
+//             while (parts.next()) |part| {
+//                 if (!validString(part)) {
+//                     err = true;
+//                     break;
+//                 }
+
+//                 const number = std.fmt.parseInt(u32, part, 10) catch continue;
+
+//                 try numList.append(number);
+//             }
+//             //   std.debug.print("extraction: {any} {any}\n", .{ err, numList.items });
+
+//             if (!err and numList.items.len == 2) {
+//                 total += (numList.items[0] * numList.items[1]);
+//                 cursor = end + 1;
+//             } else {
+//                 cursor += 1;
+//             }
+
+//             token = Token.none;
+//             continue;
+//         }
+
+//         const nexToken = getNextTokenPosition(input, cursor);
+
+//         std.debug.print("{any}\n", .{
+//             nexToken,
+//         });
+//         if (nexToken[0] == null) break;
+//         cursor = nexToken[0].?;
+
+//         const tokenOffset: usize = switch (nexToken[1]) {
+//             Token.mul => 4,
+//             Token.do => 4,
+//             Token.dont => 6,
+//             else => 1,
+//         };
+
+//         cursor += tokenOffset;
+
+//         if (!(token == Token.dont and nexToken[1] != Token.do)) {
+//             token = nexToken[1];
+//         } else {
+//             std.debug.print("Current Token: {any}, Next Token: {any}, Skipping\n", .{ token, nexToken[1] });
+//         }
+
+//         //  std.debug.print("{any} {any} {any}\n", .{ nexToken, cursor, token });
+//     }
+
+//     std.debug.print("{any}\n", .{
+//         total,
+//     });
+
+//     return total;
+// }
+
+// pub fn getNextTokenPosition(input: []const u8, cursor: usize) struct { ?usize, Token } {
+//     var lowestToken: Token = Token.none;
+//     var lowestIndex: ?usize = null;
+//     for (tokens) |iToken| {
+//         switch (iToken) {
+//             Token.do => {
+//                 const index = std.mem.indexOfPos(u8, input, cursor, "do()");
+//                 if (index == null) continue;
+//                 if (lowestIndex == null or index.? < lowestIndex.?) {
+//                     lowestIndex = index;
+//                     lowestToken = Token.do;
+//                 }
+//             },
+//             Token.dont => {
+//                 const index = std.mem.indexOfPos(u8, input, cursor, "don't()");
+//                 if (index == null) continue;
+//                 if (lowestIndex == null or index.? < lowestIndex.?) {
+//                     lowestIndex = index;
+//                     lowestToken = Token.dont;
+//                 }
+//             },
+//             Token.mul => {
+//                 const index = std.mem.indexOfPos(u8, input, cursor, "mul");
+//                 if (index == null) continue;
+//                 if (lowestIndex == null or index.? < lowestIndex.?) {
+//                     lowestIndex = index;
+//                     lowestToken = Token.mul;
+//                 }
+//             },
+//             else => {},
+//         }
+//     }
+
+//     return .{ lowestIndex, lowestToken };
+// }
+
+fn process_string(alloc: std.mem.Allocator, input: []const u8) u32 {
+    var total: u32 = 0;
+    var cursor: usize = 0;
+    //  std.debug.print("process\n", .{});
+    while (cursor < input.len) {
+        std.debug.print("cursor pos {d} {c}\n", .{ cursor, input[cursor] });
+        const start = std.mem.indexOfPos(u8, input, cursor, "mul(") orelse {
+            std.debug.print("breaking {d}\n", .{cursor});
+            break;
+        };
+
+        const ending = std.mem.indexOfPos(u8, input, start + 4, ")") orelse break;
+        //    std.debug.print("{s}\n", .{input[start..ending]});
+
+        var numberIter = std.mem.splitSequence(u8, input[start + 4 .. ending], ",");
+
+        var numList = ArrayList(u32).init(alloc);
+        defer numList.deinit();
+        var err = false;
+
+        while (numberIter.next()) |numText| {
+            if (!validString(numText)) {
+                err = true;
+                break;
+            }
+
+            const number = std.fmt.parseInt(u32, numText, 10) catch break;
+
+            numList.append(number) catch {
+                err = true;
+                break;
+            };
+        }
+
+        std.debug.print("nums {any}\n", .{
+            numList.items,
+        });
+
+        if (!err and numList.items.len == 2) {
+            total += (numList.items[0] * numList.items[1]);
+            cursor = ending + 1;
+        } else {
+            cursor += 1;
+        }
+    }
+
+    return total;
 }
